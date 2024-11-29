8c02e7c20180e7268defaa0b92ed9187dbac38c9
Rodrigo <00001103861074sp@al.educacao.sp.gov.br>
2024-11-29 01:16:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital da Dinamarca?',
+    'Copenhague'
+)
+
+criaCartao(
+    'História',
+    'Em qual ano foi fundada a ONU?',
+    'No ano de 1945'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é um quadrúpede?',
+    'Quadrúpede é um ser de 4 (quatro) pés'
+)
+
+criaCartao(
+    'Lingua Iglesa',
+    'Como se fala Cansado em inglês?',
+    'Se diz Tired'
+)
